c534505599191ea59a12ed2c410c1ec42df6ec91
meguerroa <meguerreroa@unal.edu.co>
2019-10-10 22:54:20 -0500

    Fixed reservation.js

--- controllers/v1/reservation.js
+++ controllers/v1/reservation.js
@@ -2,15 +2,15 @@ const mysql = require('mysql');
 
 
 connection = mysql.createConnection({
-    /**host            : process.env.DATABASE_HOST,
+    host            : process.env.DATABASE_HOST,
     port            : process.env.MYSQL_PORT,
     user            : process.env.MYSQL_USER,
     password        : process.env.MYSQL_PASSWORD,
-    database        : process.env.MYSQL_DATABASE**/
-    host: 'localhost',
+    database        : process.env.MYSQL_DATABASE
+    /**host: 'localhost',
     user: 'root',
     password: 'Estiven0402+',
-    database: 'reservations'
+    database: 'reservations'**/
 });
 
 let ReservationModel = {};
